6832365f9659a26aadd0019f1d721a634322cded
Oscar Bellechasse <obellech10@gmail.com>
2019-07-15 19:17:26 -0600

    Add feature test for Registration Email must be unique
    Co-Authored-By: S. Mainar <mainars09@alumni.hanover.edu>
    Co-Authored-By: Kelly Sandeson <kellysandeson@gmail.com>

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -43,5 +43,27 @@ RSpec.describe "User Registration", type: :feature do
       click_button "Create User"
       expect(page).to have_content("Name can't be blank and City can't be blank")
     end
+
+    it 'I see a flash message when I do not have a unique email' do
+      tom = User.create!(name: "Tom", address: "123 Main st", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test123" )
+
+      visit home_path
+
+      click_link "Register"
+      expect(current_path).to eq(register_path)
+
+      fill_in "Name", with: "Sam"
+      fill_in "Address", with: "1331 17th St."
+      fill_in "City", with: "Denver"
+      fill_in "State", with: "CO"
+      fill_in "Zip", with: 80202
+      fill_in "User Name", with: "iam@gmail.com"
+      fill_in "Password", with: "test"
+      fill_in "Confirm Password", with: "test"
+
+      click_button "Create User"
+
+      expect(page).to have_content("User name has already been taken")
+    end
   end
 end
