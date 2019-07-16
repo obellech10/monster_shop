1c8ce39d686040c2809bd819f434415cc3a4dc71
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-15 18:06:21 -0600

    Add test and validations to User model
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :user_name }
+  end
+end
