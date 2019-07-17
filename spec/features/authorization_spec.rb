68be989b0c88c99d06d73289bcde056eb0ab3bb2
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-17 00:46:38 -0600

    Add feature test for User Navigation Restrictions

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Navigation Restrictions", type: :feature do
       expect(page).to have_content("The page you were looking for doesn't exist.")
       expect(page.status_code).to eq(404)
     end
-  
+
     it "When I try to access any path that begins with '/profile', I see a 404 error" do
       visit profile_path
 
@@ -65,6 +65,28 @@ RSpec.describe "Navigation Restrictions", type: :feature do
       click_button "Login"
 
       visit profile_path
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+      expect(page.status_code).to eq(404)
+    end
+  end
+
+  context "As a (Registered) User" do
+    before(:each) do
+      @user = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it "When I try to access any path that begins with '/merchant', I see a 404 error" do
+      visit "/merchant"
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+      expect(page.status_code).to eq(404)
+    end
+
+    it "When I try to access any path that begins with '/admin', I see a 404 error" do
+      visit "/admin"
 
       expect(page).to have_content("The page you were looking for doesn't exist.")
       expect(page.status_code).to eq(404)
