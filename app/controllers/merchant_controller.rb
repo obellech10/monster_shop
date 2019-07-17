1b697787755f611304d97be1bdbe27ea16be1643
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-17 00:47:50 -0600

    Update show method in MerchantController to restrict user access

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -1,6 +1,6 @@
 class MerchantController < ApplicationController
   def show
-    if !current_user
+    if !current_user || current_user.default?
       render file: "/public/404", status: 404
     end
   end
