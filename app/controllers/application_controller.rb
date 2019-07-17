db6621bed637ff9ad89532d7a8c3652f5d598fdf
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-17 07:27:20 -0600

    Add methods in ApplicationController: current_admin?, current_merchant?, require_admin, require_merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,22 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_merchant?
+    current_user && current_user.merchant?
+  end
+
+  def require_admin
+    render file: 'public/404', status: 404 unless current_admin?
+  end
+
+  def require_merchant
+    render file: 'public/404', status: 404 unless current_merchant?
+  end
+
   def generate_flash(resource)
     resource.errors.messages.each do |validation, message|
       flash[validation] = "#{validation}: #{message}"
