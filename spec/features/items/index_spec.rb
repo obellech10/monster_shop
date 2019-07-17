22e6b6bd68297945deb84850245cf9552cc0cd33
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-17 11:03:49 -0600

    Create test for inactive items in item index
    Co-Authored-By: Oscar Bellechesse <obellech10@gmail.com>
    Co-Authored-By: Stella Mainar <mainars09@alumni.hanover.edu>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -9,15 +9,21 @@ RSpec.describe 'Item Index Page' do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @lamp = @megan.items.create!(name: 'Lamp', description: "I'm a Lamp!", price: 200, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: false, inventory: 10 )
     end
     it 'I can see a list of all items' do
       visit '/items'
+
+      expect(page).to_not have_link(@lamp.name)
+      expect(page).to_not have_content(@lamp.description)
+      expect(page).to_not have_content("Price: #{number_to_currency(@lamp.price)}")
+      expect(page).to_not have_content("Inactive")
+      expect(page).to_not have_content("Inventory: #{@lamp.inventory}")
 
       within "#item-#{@ogre.id}" do
         expect(page).to have_link(@ogre.name)
         expect(page).to have_content(@ogre.description)
         expect(page).to have_content("Price: #{number_to_currency(@ogre.price)}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@ogre.inventory}")
         expect(page).to have_content("Sold by: #{@megan.name}")
         expect(page).to have_css("img[src*='#{@ogre.image}']")
@@ -28,7 +34,6 @@ RSpec.describe 'Item Index Page' do
         expect(page).to have_link(@giant.name)
         expect(page).to have_content(@giant.description)
         expect(page).to have_content("Price: #{number_to_currency(@giant.price)}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@giant.inventory}")
         expect(page).to have_content("Sold by: #{@megan.name}")
         expect(page).to have_css("img[src*='#{@giant.image}']")
@@ -39,8 +44,6 @@ RSpec.describe 'Item Index Page' do
         expect(page).to have_link(@hippo.name)
         expect(page).to have_content(@hippo.description)
         expect(page).to have_content("Price: #{number_to_currency(@hippo.price)}")
-        expect(page).to have_content("Active")
-        expect(page).to have_content("Inventory: #{@hippo.inventory}")
         expect(page).to have_content("Sold by: #{@brian.name}")
         expect(page).to have_css("img[src*='#{@hippo.image}']")
         expect(page).to have_link(@brian.name)
