1b1293a53f133650ec70af9767dd950a306214a1
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-16 18:32:20 -0600

    Update feature test for User can Login to check for Admin and Merchant users
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "User can Login", type: :feature do
   describe "As a visitor, when I visit the login path" do
     context "I see a field to enter my email address and password" do
       it "If I am a Regular User, I am redirected to my profile page" do
-        user = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test")
+        user = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test", role: 0)
 
         visit login_path
 
@@ -15,6 +15,34 @@ RSpec.describe "User can Login", type: :feature do
 
         expect(current_path).to eq(profile_path)
         expect(page).to have_content("#{user.name} is logged in.")
+      end
+
+      it "If I am a Merchant User, I am redirected to my merchant dashboard page" do
+        merchant = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test", role: 2)
+
+        visit login_path
+
+        fill_in "User Name", with: "iam@gmail.com"
+        fill_in "Password", with: "test"
+
+        click_button "Login"
+
+        expect(current_path).to eq("/merchant")
+        expect(page).to have_content("#{merchant.name} is logged in.")
+      end
+
+      it "If I am a Admin User, I am redirected to my admin dashboard page" do
+        admin = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test", role: 1)
+
+        visit login_path
+
+        fill_in "User Name", with: "iam@gmail.com"
+        fill_in "Password", with: "test"
+
+        click_button "Login"
+
+        expect(current_path).to eq("/admin")
+        expect(page).to have_content("#{admin.name} is logged in.")
       end
     end
 
