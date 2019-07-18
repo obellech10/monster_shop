93f0ed0deb0eab667e5e81a54cdd762ee907a61d
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-18 13:01:01 -0600

    Beginning active record calls before post lesson updates.

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,34 @@ class Item < ApplicationRecord
     reviews.average(:rating)
   end
 
+  def bottom_five_quantity
+    b_five_quant = OrderItem.all.order(:quantity).select(:quantity).limit(5).pluck(:quantity).flatten
+  end
+
+  def bottom_five_names
+    bottom_five = OrderItem.all.order(:quantity).select(:item_id).limit(5).pluck(:item_id)
+    b_five_names = Item.select(:name).where(id: bottom_five)
+  end
+
+  def top_five_quanitiy
+    t_five_quant = OrderItem.all.order(:quantity).reverse_order.select(:quantity).limit(5).pluck(:quantity).flatten
+  end
+
+  def self.top_five_names
+    top_five = OrderItem.all.order(:quantity).reverse.order.select(:item_id).limit(5).pluck(:item_id)
+    t_five_names = Item.select(:name).where(id: top_five)
+  end
+
+  def self.top_five
+    t_five_names = top_five_names
+    t_five_quant = top_five_quanitiy
+    Hash[t_five_names.zip(t_five_quant.map {|i| i.include?(',') ? (i.split /, /) : i})]
+  end
+
+  def bottom_five
+    b_five_names = Item.bottom_five_names
+    b_five_quant = OrderItem.bottom_five_quantity
+    Hash[b_five_names.zip(b_five_quant.map {|i| i.include?(',') ? (i.split /, /) : i})]
+  end
+
 end
