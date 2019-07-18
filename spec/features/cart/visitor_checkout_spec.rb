2af976ad08759f16b315772a9bd34168f1c83f5b
S. Mainar <43945779+smainar@users.noreply.github.com>
2019-07-18 17:00:51 -0600

    Add white space on line 11

--- spec/features/cart/visitor_checkout_spec.rb
+++ spec/features/cart/visitor_checkout_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Cart Show Page' do
     @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
     @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
   end
+  
   describe 'As a Visitor' do
     describe "When I have items in my cart, and I visit my cart" do
       it "I see information telling me I must register or log in to finish the checkout process" do
