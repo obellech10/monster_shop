3ccf97163cbac01245bfffe2d242c90279e18639
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-19 18:04:26 -0600

    Remove show method and update create method in OrdersController
    Co-Authored-By: Oscar Bellechasse <obellech10@gmail.com>
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,21 @@
 class OrdersController < ApplicationController
-  def show
-    @order = Order.find(params[:id])
-  end
-
   def new
   end
 
   def create
-    order = Order.new(order_params)
-    if order.save
+    user = current_user
+    @order = user.orders.new(order_params)
+    if @order.save
       cart.items.each do |item|
-        order.order_items.create({
+        @order.order_items.create({
           item: item,
           quantity: cart.count_of(item.id),
           price: item.price
           })
       end
+      flash[:notice] = "Your order has been created."
       session.delete(:cart)
-      redirect_to order_path(order)
+      redirect_to "/profile/orders"
     else
       flash[:notice] = "Please complete address form to create an order."
       render :new
@@ -27,6 +25,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:name, :address, :city, :state, :zip)
+    params.permit(:status)
   end
 end
