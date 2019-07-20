eca26b865c41a590e8f88dc53a5001244bd32bd3
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-19 18:59:27 -0600

    Create routes for links to individual orders

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show', as: :profile
 
   get '/profile/orders', to: 'users#index'
+  get '/profile/orders/:id', to: 'users#order_show'
   post '/profile/orders', to: 'orders#create'
 
   get '/profile/password/edit', to: 'users#edit_password', as: :edit_password
