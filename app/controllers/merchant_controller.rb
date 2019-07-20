113b1af81d3ec5e811964623d46616cf505da230
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-20 14:05:58 -0600

    Add show method to merchant controller for dashboard page

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -1,6 +1,12 @@
 class MerchantController < ApplicationController
   before_action :require_merchant
-  
+
   def show
+    unless current_user && current_user.merchant?
+      render file: "/public/404", status: 404
+    else
+      render :show
+    end
   end
+
 end
