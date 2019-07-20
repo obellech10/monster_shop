aeb2fd6a60d11fcdc1c776babd0cdbe16f445b4e
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-20 14:05:04 -0600

    Merchant dashboard test to be logged in as a merchant.

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -1,72 +1,31 @@
-require 'rails_helper'
-include ActionView::Helpers::NumberHelper
+require "rails_helper"
 
-RSpec.describe 'Merchant Show Page' do
-  describe 'As a visitor' do
+RSpec.describe "Merchant Dashboard Show Page", type: :feature do
+  describe 'As a merchant' do
     before :each do
-      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @sal = Merchant.create!(name: 'Sals Salamanders', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @merchant = User.create!(name: "Sam", address: "1331 17th St.", city: "Denver", state: "CO", zip: 80202, user_name: "iam@gmail.com", password: "test", role: 2)
 
-      @user = User.create!(name: "Diane", address: "1331 Main St.", city: "Denver", state: "CO", zip: 80202, user_name: "tom@gmail.com", password: "test", role: 0)
-      @user_2 = User.create!(name: "Sam", address: "1331 Main St.", city: "Denver", state: "IA", zip: 80202, user_name: "sam@gmail.com", password: "test", role: 0)
-
-      @order_1 = @user.orders.create!(status: 0)
-      @order_2 = @user_2.orders.create!(status: 0)
-      @order_2 = @user_2.orders.create!(status: 0)
-      @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
-      @order_2.order_items.create!(item: @giant, price: @hippo.price, quantity: 2)
-      @order_2.order_items.create!(item: @ogre, price: @hippo.price, quantity: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
     end
 
-    it 'I see merchant name and address' do
-      visit "/merchants/#{@megan.id}"
+    it 'I see all my profile details on the page and cannot edit them' do
+      visit merchant_dashboard_path
 
-      expect(page).to have_content(@megan.name)
-
-      within '.address' do
-        expect(page).to have_content(@megan.address)
-        expect(page).to have_content("#{@megan.city} #{@megan.state} #{@megan.zip}")
-      end
+      expect(current_path).to eq(merchant_dashboard_path)
+      expect(page).to have_content("#{@merchant.name}'s Profile")
+      expect(page).to have_content(@merchant.address)
+      expect(page).to have_content(@merchant.city)
+      expect(page).to have_content(@merchant.state)
+      expect(page).to have_content(@merchant.zip)
+      expect(page).to have_content(@merchant.user_name)
     end
 
-    it 'I see a link to this merchants items' do
-      visit "/merchants/#{@megan.id}"
+    it "As a regular user i cannot visit the merchant dashboard path" do
+      @user = User.create!(name: "Lisa", address: "1331 25th Ave.", city: "Miami", state: "FL", zip: 80242, user_name: "bird@gmail.com", password: "fish", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      click_link "Items"
-
-      expect(current_path).to eq("/items")
-    end
-
-    it 'I see merchant statistics' do
-      visit "/merchants/#{@megan.id}"
-
-      within '.statistics' do
-        expect(page).to have_content("Item Count: #{@megan.item_count}")
-        expect(page).to have_content("Average Item Price: #{number_to_currency(@megan.average_item_price)}")
-        expect(page).to have_content("Cities Served:\nDenver, CO\nDenver, IA")
-      end
-    end
-
-    it 'I see stats for merchants with items, but no orders' do
-      visit "/merchants/#{@brian.id}"
-
-      within '.statistics' do
-        expect(page).to have_content("Item Count: #{@brian.item_count}")
-        expect(page).to have_content("Average Item Price: #{number_to_currency(@brian.average_item_price)}")
-        expect(page).to have_content("This Merchant has no Orders!")
-      end
-    end
-
-    it 'I see stats for merchants with no items or orders' do
-      visit "/merchants/#{@sal.id}"
-
-      within '.statistics' do
-        expect(page).to have_content('This Merchant has no Items, or Orders!')
-      end
+      visit merchant_dashboard_path
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end
   end
 end
