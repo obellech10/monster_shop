1f08d4c7f6305c289d0af112784488ff19f5d88e
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-22 15:34:18 -0600

    Add validations and class methods - cancel and fulfill - for OrderItem model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,24 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+  validates_presence_of :price,
+                        :quantity
+
   def subtotal
     quantity * price
+  end
+
+  def fulfill
+    if item.inventory >= self.quantity
+      item.inventory -= self.quantity
+      self.update(fulfilled: true)
+    end
+  end
+
+  def cancel
+    if self.fulfilled?
+      self.update(fulfilled: false)
+      self.item.update(inventory: (item.inventory + self.quantity))
+    end
   end
 end
