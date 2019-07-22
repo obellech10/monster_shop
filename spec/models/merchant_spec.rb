359638e1fd0c7422afb927e10f58b6f6179eeec5
Oscar Bellechasse <obellech10@gmail.com>
2019-07-21 20:37:13 -0600

    Update variable for user creation in before each block
    Co-Authored-By: S. Mainar <mainars09@alumni.hanover.edu>

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe Merchant do
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 
-      @user_1 = User.create!(name: "Diane", address: "1331 Main St.", city: "Denver", state: "CO", zip: 80202, user_name: "tom@gmail.com", password: "test", role: 0)
-      @user_2 = User.create!(name: "Sam", address: "1331 Main St.", city: "Denver", state: "IA", zip: 80202, user_name: "sam@gmail.com", password: "test", role: 0)
+      @user_1 = User.create!(name: "Diane", address: "1331 Main St.", city: "Denver", state: "CO", zip: 80202, user_name: "tom@gmail.com", password: "test", role: 0, merchant_id: @megan.id)
+      @user_2 = User.create!(name: "Sam", address: "1331 Main St.", city: "Denver", state: "IA", zip: 80202, user_name: "sam@gmail.com", password: "test", role: 0, merchant_id: @brian.id)
 
       @order_1 = @user_1.orders.create!(status: 0)
       @order_2 = @user_2.orders.create!(status: 0)
