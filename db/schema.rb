4f9ead040960fea0bbc570c8b7614075d6de26a8
Oscar Bellechasse <obellech10@gmail.com>
2019-07-22 19:45:12 -0600

    Update order table status default to 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190721134242) do
+ActiveRecord::Schema.define(version: 20190721221603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20190721134242) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 0
+    t.integer "status", default: 1
     t.bigint "user_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
