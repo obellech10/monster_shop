3f4b0126430958f9aac4c7198b82ba3584d67cee
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-22 23:52:25 -0600

    Create order_show controller method

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -6,4 +6,9 @@ class MerchantController < ApplicationController
     @orders = Order.all
   end
 
+  def order_show
+    @merchant = Merchant.find(current_user.merchant_id)
+    @order = Order.find(params[:id])
+  end
+
 end
