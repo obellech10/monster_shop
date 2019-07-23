fc6cfa9d96047e1ea6abd4a6cf2a363c314fb506
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-23 15:41:31 -0600

    Add index and disable methods in Admin::MerchantsController

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,4 +1,16 @@
-class Admin::MerchantsController < ApplicationController
+class Admin::MerchantsController < Admin::BaseController
+  def index
+    @merchants = Merchant.all
+  end
+
   def show
+  end
+
+  def disable
+    merchant = Merchant.find(params[:id])
+    merchant.update(enabled: false)
+    # merchant.save
+    flash[:success] = "#{merchant.name}'s account is now disabled."
+    redirect_to admin_merchants_path
   end
 end
