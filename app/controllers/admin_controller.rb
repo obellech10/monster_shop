a05a625d4be89999ae1c7f45acf02c8537590fa0
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-24 09:00:41 -0600

    Create ship order method for admin use

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,11 @@ class AdminController < ApplicationController
   def show
     @orders = Order.sorted_orders
   end
+
+  def ship_order
+    order= Order.find(params[:order])
+    order.update(status: 'shipped')
+    render :show
+  end
+
 end
